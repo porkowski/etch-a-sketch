afeff76ceaf61fa0d6de33be86e98d2c9a73d3e5
porkowski <borkowskipatryk10@gmail.com>
2023-05-16 22:17:08 -0400

    Working on fixing the resizing of the grid.

--- etch.js
+++ etch.js
@@ -1,7 +1,11 @@
 //grab div container i made in html to javascript
 const divBody = document.getElementById("boxHome");
 
-
+//create 256 divs initially
+for(let i=0; i<256; i++) {
+    createDiv();
+        
+    }
 
 //Declare createDiv function, creates newDiv "boxes" and appends to divBody
 function createDiv() {
@@ -10,32 +14,32 @@ function createDiv() {
     divBody.appendChild(newDiv);
 }
 
-
-//create 256 divs
-for(let i=0; i<256; i++) {
-createDiv();
-    
-}
 const newDiv = document.querySelectorAll("div.boxes");
 
 
-
-
-
-//Mouseover eventlistener to newDiv
-newDiv.forEach(box => box.addEventListener("mouseover",(e) => {
-    e.target.classList.add("hover");
-}));
-
 //grab sizing button
 const button = document.querySelector("button");
-//on click, prompt for size. Store value in size
+//on click, prompt for size & change gridSize
 button.addEventListener("click",gridSize);
 
 function gridSize () {
 const size =prompt("Please choose a grid size between 0 and 100");
+//only work if size is between 0 and 100
+if (size >0 && size <=100) {
+//remove all children of divBody.
+while (divBody.firstChild) {
+    divBody.removeChild(divBody.lastChild);
 }
-
+//input new amount of newDivs based on the prompt selected
+for(let i=0; i<(size*size); i++) {
+    createDiv();
+    }
+} else alert("Read again! Choose a number between 0 and 100!");
+}
+//Mouseover eventlistener to newDiv
+newDiv.forEach(box => box.addEventListener("mouseover",(e) => {
+    e.target.classList.add("hover");
+}));
 
 
 /*Incase needed, this makes it so divs are not permanently grey,
